37a9311de6e383862ff20894b1b67180dc2be82b
selab <selab@gmail.com>
2021-05-26 15:35:46 +0800

    bugFix

--- temp/Temp.java
+++ temp/Temp.java
@@ -1,21 +1,14 @@
 public class Temp{
     
-@GetMapping(value = "/login", produces = "application/json")
-public Map<String, Object> login(@RequestParam(name = "username")
-String username, @RequestParam(name = "password")
-String password, HttpServletResponse response) {
-    User user = userService.authenticate(username, password);
-    for (Token token : user.getTokens()) userService.expireToken(token);
-    Token token = userService.assignNewToken(user);
-    Cookie cookie;
-    cookie = new Cookie("uid", token.getUser().getId().toString());
-    cookie.setPath("/");
-    response.addCookie(cookie);
-    cookie = new Cookie("token", token.getToken());
-    cookie.setPath("/");
-    response.addCookie(cookie);
-    Map<String, Object> body = new HashMap<>();
-    body.put("privilege", 1);
-    return body;
+@PostMapping(value = "/categories/{id}/subcategories", produces = "application/json")
+public Category addSubcategory(@CookieValue(name = "uid")
+String uid, @CookieValue(name = "token")
+String token, @PathVariable(name = "id")
+String cid, @Valid()
+@RequestBody()
+Category category, BindingResult validation) {
+    userService.isLogin(new Integer(uid), token);
+    if (validation.getFieldErrorCount() > 0) throw new UnprocessableEntityException(String.format("Invalid field value: %s", validation.getFieldErrors().get(0).getField()));
+    return hierarchyService.subcategoryInsert(cid, category);
 }
 }
