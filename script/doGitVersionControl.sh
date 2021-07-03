07824cd01a8437d8625d84fbfad8754e5ab17b85
Tim Hsieh <timhost1233@gmail.com>
2021-07-03 22:30:42 +0800

    inventory: laet

--- script/doGitVersionControl.sh
+++ script/doGitVersionControl.sh
@@ -2,6 +2,4 @@ mode=$1
 projectPath=$2
 service_name=$3
 echo $2
-cd ${projectPath}
-git add .
-git commit -m "$2: ${service_name}" 
+cd ${projectPath} || git add . && git commit -m "$2: ${service_name}"
